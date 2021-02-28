daa82c7298ac2b17b75e8575408d8cc057a5a085
Ole <refsa22@gmail.com>
2021-02-28 13:34:22 +0100

    feat: shell for PolyLine drawer

---
+++ Runtime/Drawing/Drawers/ReGizmoPolyLineDrawer.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+namespace ReGizmo.Drawing
+{
+    internal class ReGizmoPolyLineDrawer : ReGizmoDrawer<LineData>
+    {
+        protected override void RenderInternal(Camera camera)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
