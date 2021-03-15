ed848cf740c9a59d9add86eb528695d98aad8430
Ole <refsa22@gmail.com>
2021-03-15 10:51:38 +0100

    feat: more font stuff chore: added license info for 3rd party tools

---
+++ Samples/DrawSDFText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ReGizmo.Drawing;
+using UnityEngine;
+
+public class DrawSDFText : MonoBehaviour
+{
+    const string text = "Hello";
+
+    protected void RunInternal()
+    {
+        for (int x = 0; x < 64; x++)
+        {
+            for (int y = 0; y < 64; y++)
+            {
+                ReDraw.TextSDF(text, new Vector3(x, 0, y) * 2, 1f, Color.green);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        RunInternal();
+    }
+}
