14789f57edbc6a798234db3dc9ece099ba857047
Ole <refsa22@gmail.com>
2021-05-06 13:43:53 +0200

    sample: added sprite drawing sample + spritesheet

---
+++ Samples/DrawSprites.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using ReGizmo.Drawing;
+using UnityEngine;
+
+namespace ReGizmo.Samples
+{
+    public class DrawSprites : MonoBehaviour
+    {
+        [SerializeField] List<Sprite> sprites;
+
+        void OnDrawGizmos()
+        {
+            if (sprites == null) return;
+
+            using (new TransformScope(transform))
+            {
+                int index = 0;
+                foreach (var sprite in sprites)
+                {
+                    ReDraw.Sprite(sprite, Vector3.up * 12 * index++, 4f);
+                }
+            }
+        }
+    }
+}
