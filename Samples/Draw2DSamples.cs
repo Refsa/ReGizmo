a4277979181fba566ebfbd9985696dd9d8582e98
Ole <refsa22@gmail.com>
2021-05-08 15:45:21 +0200

    sample: added samples for 2D drawing of triangle/circle

---
+++ Samples/Draw2DSamples.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ReGizmo.Drawing;
+using UnityEngine;
+
+public class Draw2DSamples : MonoBehaviour
+{
+    [SerializeField, Range(1f, 1024f)] float circleOuterRadus = 128f;
+    [SerializeField, Range(1f, 1024f)] float circleInnerRadius = 6f;
+
+    void OnDrawGizmos()
+    {
+        ReDraw.Circle(Vector3.zero, Vector3.up, circleOuterRadus, circleInnerRadius, Color.red);
+
+        ReDraw.Triangle(Vector3.right * 5f, Vector3.up, circleOuterRadus, 0f, Color.green);
+    }
+}
