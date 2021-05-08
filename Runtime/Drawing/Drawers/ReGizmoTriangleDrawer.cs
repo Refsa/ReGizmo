46cb351dc2a5235c6d3aa998d032be45b05acf87
Ole <refsa22@gmail.com>
2021-05-08 15:44:50 +0200

    feat: Added an SDF Triangle drawer

---
+++ Runtime/Drawing/Drawers/ReGizmoTriangleDrawer.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ReGizmo.Drawing
+{
+    internal struct TriangleDrawData
+    {
+        public Vector3 Position;
+        public Vector3 Normal;
+        public float Width;
+        public float Thickness;
+        public Vector3 Color;
+    }
+
+    internal class ReGizmoTriangleDrawer : ReGizmoDrawer<TriangleDrawData>
+    {
+        public ReGizmoTriangleDrawer() : base()
+        {
+            material = new Material(ReGizmoHelpers.LoadShader("Hidden/ReGizmo/TriangleShader"));
+            renderArguments[1] = 1;
+        }
+
+        protected override void RenderInternal(CommandBuffer cmd)
+        {
+            renderArguments[0] = CurrentDrawCount();
+            renderArgumentsBuffer.SetData(renderArguments);
+
+            cmd.DrawProceduralIndirect(
+                Matrix4x4.identity,
+                material, 0,
+                MeshTopology.Points,
+                renderArgumentsBuffer, 0,
+                materialPropertyBlock
+            );
+        }
+    }
+}
