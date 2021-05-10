b5ddc5b28ca86206c00f4c2c4b7e00f07df49a24
Ole <refsa22@gmail.com>
2021-05-10 08:51:03 +0200

    feat: added in types for handling scale and draw mode

--- Runtime/Drawing/ReDraw.cs
+++ Runtime/Drawing/ReDraw.cs
@@ -4,6 +4,41 @@ using UnityEngine;
 
 namespace ReGizmo.Drawing
 {
+    public enum DrawMode : byte
+    {
+        BillboardFree,
+        BillboardAligned,
+        AxisAligned,
+    }
+
+    internal enum ScaleType : byte
+    {
+        Pixel = 0,
+        Percent = 1,
+        Unit = 2,
+    }
+
+    public struct Scale
+    {
+        public byte ScaleMode;
+        public float Value;
+
+        public static Scale Pixels(float pixels)
+        {
+            return new Scale { ScaleMode = (byte)ScaleType.Pixel, Value = pixels };
+        }
+
+        public static Scale Percent(float percent)
+        {
+            return new Scale { ScaleMode = (byte)ScaleType.Percent, Value = Mathf.Clamp01(percent) };
+        }
+
+        public static Scale Units(float units)
+        {
+            return new Scale { ScaleMode = (byte)ScaleType.Unit, Value = units };
+        }
+    }
+
     /// <summary>
     /// Other partial implementations only contains extension methods for the different drawing calls
     /// </summary>
