33bd8c3c5a09349262c0f6774682a122b6b09fce
Ole <refsa22@gmail.com>
2021-05-10 13:48:42 +0200

    refactor: moved common part of genrator into base

--- Editor/Generator/DrawGenerator.cs
+++ Editor/Generator/DrawGenerator.cs
@@ -5,7 +5,7 @@ namespace ReGizmo.Generator
 {
     internal abstract class DrawGenerator
     {
-        protected string fileShell = 
+        protected string fileShell =
 @"
 using UnityEngine;
 
@@ -31,5 +31,31 @@ $CONTENT
         }
 
         protected abstract string GenerateInternal();
+
+        protected string GenerateHelper(string defaultParams)
+        {
+            string content = "";
+
+            foreach (var perm in Permutation.GenerateOverrides(variables))
+            {
+                string method = methodShell;
+                string parameters = defaultParams;
+                if (!string.IsNullOrEmpty(perm.Item2))
+                {
+                    if (!string.IsNullOrEmpty(parameters)) parameters += ", ";
+                    parameters += perm.Item2;
+                }
+                method = method.Replace("$PARAMS", parameters);
+
+                string[] chars = perm.Item1.Split(',');
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    method = method.Replace($"$PARAM_{i + 1}", chars[i].Trim());
+                }
+                content += method;
+            }
+
+            return content;
+        }
     }
 }
