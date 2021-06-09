3ad73b957683ce59a450cbca4e24c8414ca7f160
Ole <refsa22@gmail.com>
2021-06-09 22:21:45 +0200

    sample: added an LTree as a sample and perf test

---
+++ Samples/LTreeSample/FernLTree.cs
@@ -0,0 +1,197 @@
+using System.Collections.Generic;
+using ReGizmo.Drawing;
+using UnityEngine;
+
+namespace ReGizmo.Samples
+{
+    public struct FernState : IState<FernMode>
+    {
+        public Vector3 Pos;
+        public Vector3 Dir;
+        public FernMode Mode { get; set; }
+
+        public FernState(Vector3 pos, Vector3 dir, FernMode mode)
+        {
+            Pos = pos;
+            Dir = dir;
+            Mode = mode;
+        }
+
+
+        public IState<FernMode> Draw(IState<FernMode> prevState, Stack<IState<FernMode>> states)
+        {
+            IState<FernMode> nextState = this;
+
+            switch (Mode)
+            {
+                case FernMode.DrawForward:
+                    var ps = (FernState)prevState;
+                    ReDraw.Line(ps.Pos, Pos, Color.green, 1f);
+                    break;
+                case FernMode.Save:
+                    states.Push(this);
+                    break;
+                case FernMode.Load:
+                    var mode = Mode;
+                    nextState = states.Pop();
+                    nextState.Mode = mode;
+                    break;
+            }
+
+            return nextState;
+        }
+    }
+
+    public enum FernMode
+    {
+        Unknown = 0,
+        Expand,
+        DrawForward,
+        TurnRight,
+        TurnLeft,
+        Save,
+        Load,
+    }
+
+    struct ExpandRule : IRule<FernState, FernMode>
+    {
+        public FernMode RuleFor => FernMode.Expand;
+
+        public FernState Build(FernState current, Stack<FernState> stack)
+        {
+            return current;
+        }
+
+        public LinkedListNode<FernMode> Generate(LL<FernMode> ltree, LinkedListNode<FernMode> current)
+        {
+            current = ltree.AddAfter(current, FernMode.DrawForward);
+            current = ltree.AddAfter(current, FernMode.TurnLeft);
+            current = ltree.AddAfter(current, FernMode.Save);
+            current = ltree.AddAfter(current, FernMode.Save);
+            current = ltree.AddAfter(current, FernMode.Expand);
+            current = ltree.AddAfter(current, FernMode.Load);
+            current = ltree.AddAfter(current, FernMode.TurnRight);
+            current = ltree.AddAfter(current, FernMode.Expand);
+            current = ltree.AddAfter(current, FernMode.Load);
+            current = ltree.AddAfter(current, FernMode.TurnRight);
+            current = ltree.AddAfter(current, FernMode.DrawForward);
+            current = ltree.AddAfter(current, FernMode.Save);
+            current = ltree.AddAfter(current, FernMode.TurnRight);
+            current = ltree.AddAfter(current, FernMode.DrawForward);
+            current = ltree.AddAfter(current, FernMode.Expand);
+            current = ltree.AddAfter(current, FernMode.Load);
+            current = ltree.AddAfter(current, FernMode.TurnLeft);
+            current = ltree.AddAfter(current, FernMode.Expand);
+            return current;
+        }
+    }
+
+    struct DrawForwardRule : IRule<FernState, FernMode>
+    {
+        public FernMode RuleFor => FernMode.DrawForward;
+
+        public FernState Build(FernState current, Stack<FernState> stack)
+        {
+            current.Pos += current.Dir;
+            return current;
+        }
+
+        public LinkedListNode<FernMode> Generate(LL<FernMode> ltree, LinkedListNode<FernMode> current)
+        {
+            current = ltree.AddAfter(current, FernMode.DrawForward);
+            current = ltree.AddAfter(current, FernMode.DrawForward);
+            return current;
+        }
+    }
+
+    struct TurnRightRule : IRule<FernState, FernMode>
+    {
+        public FernMode RuleFor => FernMode.TurnRight;
+
+        public FernState Build(FernState current, Stack<FernState> stack)
+        {
+            current.Dir = Quaternion.Euler(-Random.Range(15f, 30f), 0f, -Random.Range(15f, 30f)) * current.Dir;
+            return current;
+        }
+
+        public LinkedListNode<FernMode> Generate(LL<FernMode> ltree, LinkedListNode<FernMode> current)
+        {
+            return current;
+        }
+    }
+
+    struct TurnLeftRule : IRule<FernState, FernMode>
+    {
+        public FernMode RuleFor => FernMode.TurnLeft;
+
+        public FernState Build(FernState current, Stack<FernState> stack)
+        {
+            current.Dir = Quaternion.Euler(Random.Range(15f, 30f), 0f, Random.Range(15f, 30f)) * current.Dir;
+            return current;
+        }
+
+        public LinkedListNode<FernMode> Generate(LL<FernMode> ltree, LinkedListNode<FernMode> current)
+        {
+            return current;
+        }
+    }
+
+    struct SaveRule : IRule<FernState, FernMode>
+    {
+        public FernMode RuleFor => FernMode.Save;
+
+        public FernState Build(FernState current, Stack<FernState> stack)
+        {
+            stack.Push(current);
+            return current;
+        }
+
+        public LinkedListNode<FernMode> Generate(LL<FernMode> ltree, LinkedListNode<FernMode> current)
+        {
+            return current;
+        }
+    }
+
+    struct LoadRule : IRule<FernState, FernMode>
+    {
+        public FernMode RuleFor => FernMode.Load;
+
+        public FernState Build(FernState current, Stack<FernState> stack)
+        {
+            var state = stack.Pop();
+            state.Mode = current.Mode;
+            return state;
+        }
+
+        public LinkedListNode<FernMode> Generate(LL<FernMode> ltree, LinkedListNode<FernMode> current)
+        {
+            return current;
+        }
+    }
+
+    public class FernLTree : LTree<FernState, FernMode>
+    {
+        protected override System.Func<FernState, FernMode, FernState> stateFactory => StateFactory;
+        protected override FernState rootState => new FernState(Vector3.zero, Vector3.up, FernMode.Unknown);
+
+        public FernLTree() : base()
+        {
+            rules.Add(FernMode.Expand, new ExpandRule());
+            rules.Add(FernMode.DrawForward, new DrawForwardRule());
+            rules.Add(FernMode.TurnRight, new TurnRightRule());
+            rules.Add(FernMode.TurnLeft, new TurnLeftRule());
+            rules.Add(FernMode.Save, new SaveRule());
+            rules.Add(FernMode.Load, new LoadRule());
+        }
+
+        FernState StateFactory(FernState state, FernMode mode)
+        {
+            return new FernState(state.Pos, state.Dir, mode);
+        }
+
+        protected override void PreGenerate()
+        {
+            ltree.AddFirst(FernMode.Expand);
+        }
+    }
+}
---
+++ Samples/LTreeSample/LTree.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReGizmo.Samples
+{
+    public class LL<T> : System.Collections.Generic.LinkedList<T>
+    {
+        public LinkedListNode<T> FindAfter(LinkedListNode<T> current, System.Func<T, bool> predicate)
+        {
+            if (current.Next == null) return null;
+
+            return FindAfterInternal(current.Next, predicate);
+        }
+
+        LinkedListNode<T> FindAfterInternal(LinkedListNode<T> current, System.Func<T, bool> predicate)
+        {
+            if (predicate.Invoke(current.Value))
+            {
+                return current;
+            }
+
+            if (current.Next == null) return null;
+            return FindAfterInternal(current.Next, predicate);
+        }
+    }
+
+    public interface IState<TMode>
+        where TMode : Enum
+    {
+        TMode Mode { get; set; }
+
+        IState<TMode> Draw(IState<TMode> prevState, Stack<IState<TMode>> states);
+    }
+
+    public interface IRule<TState, TMode>
+        where TState : struct, IState<TMode>
+        where TMode : Enum
+    {
+        TMode RuleFor { get; }
+        TState Build(TState current, Stack<TState> stack);
+        LinkedListNode<TMode> Generate(LL<TMode> ltree, LinkedListNode<TMode> current);
+    }
+
+    public interface ILTree
+    {
+        void Generate(int generations);
+        void Build();
+        void Draw();
+    }
+
+    public abstract class LTree<TState, TMode> : ILTree
+        where TState : struct, IState<TMode>
+        where TMode : Enum
+    {
+        protected Dictionary<TMode, IRule<TState, TMode>> rules;
+        protected LL<TMode> ltree;
+
+        protected LL<TState> generated;
+        protected Stack<TState> buildStates;
+        protected Stack<IState<TMode>> drawStates;
+
+        protected abstract System.Func<TState, TMode, TState> stateFactory { get; }
+        protected abstract TState rootState { get; }
+
+        public LTree()
+        {
+            rules = new Dictionary<TMode, IRule<TState, TMode>>();
+            ltree = new LL<TMode>();
+            generated = new LL<TState>();
+            buildStates = new Stack<TState>();
+            drawStates = new Stack<IState<TMode>>();
+        }
+
+        public void Generate(int generations)
+        {
+            ltree.Clear();
+            PreGenerate();
+
+            for (int i = 0; i < generations; i++)
+            {
+                foreach (var rule in rules.Values)
+                {
+                    var current = ltree.Find(rule.RuleFor);
+                    while (current != null)
+                    {
+                        var next = rule.Generate(ltree, current);
+
+                        ltree.Remove(current);
+                        current = ltree.FindAfter(next, m => m.Equals(rule.RuleFor));
+                    }
+                }
+            }
+        }
+
+        protected abstract void PreGenerate();
+
+        public void Build()
+        {
+            generated.Clear();
+            buildStates.Clear();
+
+            TState prevState = rootState;
+            var current = ltree.First;
+
+            while (current != null)
+            {
+                TState currState = stateFactory.Invoke(prevState, current.Value);
+
+                if (rules.TryGetValue(current.Value, out var rule))
+                {
+                    currState = rule.Build(currState, buildStates);
+
+                    generated.AddLast(currState);
+                    prevState = currState;
+                }
+
+                current = current.Next;
+            }
+        }
+
+        public void Draw()
+        {
+            if (generated.First == null || generated.First.Next == null) return;
+
+            var prevState = generated.First.Value;
+            var currNode = generated.First.Next;
+
+            while (currNode != null)
+            {
+                var state = currNode.Value;
+                prevState = (TState)state.Draw((IState<TMode>)prevState, drawStates);
+
+                currNode = currNode.Next;
+            }
+        }
+    }
+}
