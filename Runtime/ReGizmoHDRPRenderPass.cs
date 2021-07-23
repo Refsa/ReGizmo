680cf93f6d8ce3471ae90284c2c24696f65b7af6
Ole <refsa22@gmail.com>
2021-07-23 20:19:13 +0200

    fix: HDRP render pass now works properly

--- Runtime/ReGizmoHDRPRenderPass.cs
+++ Runtime/ReGizmoHDRPRenderPass.cs
@@ -8,22 +8,37 @@ namespace ReGizmo.Core.HDRP
 {
     class ReGizmoHDRPRenderPass : CustomPass
     {
-        public static event System.Action<CommandBuffer, Camera> OnPassExecute;
+        public delegate void PassExecuteDelegate(in ScriptableRenderContext context, CommandBuffer cmd, Camera camera, in Framebuffer framebuffer);
+
+        public static event PassExecuteDelegate OnPassExecute;
         public static event System.Action OnPassCleanup;
-    
+
+        Material copyFramebufferMaterial;
+        Material clearMaterial;
+
         protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
         {
-        
+            name = "ReGizmoHDRPPass";
+
+            clearMaterial = CoreUtils.CreateEngineMaterial("Hidden/ReGizmo/Clear");
+            copyFramebufferMaterial = CoreUtils.CreateEngineMaterial("Hidden/ReGizmo/CopyFramebuffer");
         }
-    
+
         protected override void Execute(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera hdCamera, CullingResults cullingResult)
         {
-            OnPassExecute?.Invoke(cmd, hdCamera.camera);
+            SetCameraRenderTarget(cmd);
+            GetCameraBuffers(out var color, out var depth);
+            var framebuffer = new Framebuffer(color, depth);
+
+            OnPassExecute?.Invoke(renderContext, cmd, hdCamera.camera, framebuffer);
         }
-    
+
         protected override void Cleanup()
         {
             OnPassCleanup?.Invoke();
+
+            CoreUtils.Destroy(copyFramebufferMaterial);
+            CoreUtils.Destroy(clearMaterial);
         }
     }
 }
