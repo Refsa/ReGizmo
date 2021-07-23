d82c66064d37995bbe6c590dc512a3cee4779e1e
Ole <refsa22@gmail.com>
2021-07-23 20:19:00 +0200

    feat: added HDRP version of Framebuffer to support RTHandle

--- Runtime/Framebuffer.cs
+++ Runtime/Framebuffer.cs
@@ -5,7 +5,25 @@ namespace ReGizmo
 {
     internal struct Framebuffer
     {
+#if RG_HDRP
+        public RTHandle ColorTarget;
+        public RTHandle DepthTarget;
+
+        public Framebuffer(RTHandle colorTarget, RTHandle depthTarget)
+        {
+            ColorTarget = colorTarget;
+            DepthTarget = depthTarget;
+        }
+#else
         public RenderTargetIdentifier ColorTarget;
         public RenderTargetIdentifier DepthTarget;
+
+        public Framebuffer(RenderTargetIdentifier colorTarget, RenderTargetIdentifier depthTarget)
+        {
+            ColorTarget = colorTarget;
+            DepthTarget = depthTarget;
+        }
+#endif
+
     }
 }
