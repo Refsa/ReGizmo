abc48b6fc2cf1fc9c6225e0b65bafc3bcc857dc9
Ole <refsa22@gmail.com>
2021-08-12 08:43:46 +0200

    feat: added clear and copy depth to ShaderUtils

--- Runtime/Utils/ShaderUtils.cs
+++ Runtime/Utils/ShaderUtils.cs
@@ -6,12 +6,16 @@ namespace ReGizmo
     internal static class ShaderUtils
     {
         static Material clearMaterial;
+        static Material clearDepthMaterial;
         static Material clearWithDepthMaterial;
+        static Material copyDepthMaterial;
 
         static ShaderUtils()
         {
             clearMaterial = new Material(Shader.Find("Hidden/ReGizmo/Clear"));
+            clearDepthMaterial = new Material(Shader.Find("Hidden/ReGizmo/ClearDepth"));
             clearWithDepthMaterial = new Material(Shader.Find("Hidden/ReGizmo/ClearWithDepth"));
+            copyDepthMaterial = new Material(Shader.Find("Hidden/ReGizmo/CopyDepth"));
         }
 
         public static void ClearTexture(CommandBuffer cmd, RenderTargetIdentifier texture, Color clearColor)
@@ -32,6 +36,18 @@ namespace ReGizmo
             cmd.SetGlobalColor("_ClearColor", clearColor);
             cmd.SetGlobalFloat("_ClearDepth", depth);
             cmd.Blit(null, texture, clearWithDepthMaterial);
+        }
+
+        public static void ClearDepth(CommandBuffer cmd, RenderTargetIdentifier texture, float depth)
+        {
+            cmd.SetGlobalFloat("_ClearDepth", depth);
+            cmd.Blit(null, texture, clearDepthMaterial);
+        }
+
+        public static void CopyDepth(CommandBuffer cmd, RenderTargetIdentifier targetTexture, RenderTargetIdentifier depthTexture)
+        {
+            cmd.SetGlobalTexture("_MainTex", depthTexture);
+            cmd.Blit(depthTexture, targetTexture, copyDepthMaterial);
         }
     }
 }
